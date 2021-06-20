980ea1fe71e1c74964c525e9c2a22fa47e5d5e80
Mohmad012 <mohmadailsia@gmail.com>
2021-06-20 22:38:47 +0200

    finish skills page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,123 +19,21 @@ import Tech from "./imgs/tec.jpg"
 const Projects = () => {
 
 	const allData = [
-		{
-			id:1,
-			img:amazon,
-			linkLive:"https://amazon-app-self.vercel.app/",
-			linkGithub:"https://github.com/Mohmad012/Amazon_App",
-			name:"Amazon App",
-			frontBack:false
-		},
-		{
-			id:2,
-			img:tiktok,
-			linkLive:"https://tiktok-f8fc2.web.app/",
-			frontGithub:"https://github.com/Mohmad012/TikTok_frontend",
-			backGithub:"https://github.com/Mohmad012/TikTok_backend",
-			name:"TikTok App",
-			frontBack:true
-		},
-		{
-			id:3,
-			img:tinder,
-			linkLive:"https://mohmad012.github.io/Tinder_frontend/",
-			frontGithub:"https://github.com/Mohmad012/Tinder_frontend",
-			backGithub:"https://github.com/Mohmad012/Tinder_backend",
-			name:"Tinder App",
-			frontBack:true
-		},
-		{
-			id:4,
-			img:shopStore,
-			linkLive:"https://store-shop-phi.vercel.app/",
-			linkGithub:"https://github.com/Mohmad012/Store_Shop",
-			name:"Shop Store",
-			frontBack:false
-		},
-		{
-			id:5,
-			img:TV,
-			linkLive:"https://mohmad012.github.io/TV_Search_Flix/",
-			linkGithub:"https://github.com/Mohmad012/TV_Search_Flix",
-			name:"TV Search Flims",
-			frontBack:false
-		},
-		{
-			id:6,
-			img:Todo,
-			linkLive:"https://mohmad012.github.io/TodoList/",
-			linkGithub:"https://github.com/Mohmad012/TodoList",
-			name:"Todo List",
-			frontBack:false
-		},
-		{
-			id:7,
-			img:Bookmark,
-			linkLive:"./Projects/Bookmark/index.html",
-			linkGithub:"https://github.com/Mohmad012/Portfolio/tree/main/Projects/Bookmark",
-			name:"Bookmark Site",
-			frontBack:false
-		},
-		{
-			id:8,
-			img:Car,
-			linkLive:"https://mohmad012.github.io/car-site/",
-			linkGithub:"https://github.com/Mohmad012/car-site",
-			name:"Car Site",
-			frontBack:false
-		},
-		{
-			id:9,
-			img:Composition,
-			linkLive:"https://mohmad012.github.io/Sass_Pugjs_Project/",
-			linkGithub:"https://github.com/Mohmad012/Sass_Pugjs_Project",
-			name:"Composition Site",
-			frontBack:false
-		},
-		{
-			id:10,
-			img:Special,
-			linkLive:"https://mohmad012.github.io/web_pure_Javascript/",
-			linkGithub:"https://github.com/Mohmad012/web_pure_Javascript",
-			name:"Special Design",
-			frontBack:false
-		},
-		{
-			id:11,
-			img:Elite,
-			linkLive:"https://mohmad012.github.io/Bootstrap4/",
-			linkGithub:"https://github.com/Mohmad012/Bootstrap4",
-			name:"Elite Corp Site",
-			frontBack:false
-		},
-		{
-			id:12,
-			img:Prototypes,
-			linkLive:"https://mohmad012.github.io/bootstrap3-2/",
-			linkGithub:"https://github.com/Mohmad012/bootstrap3-2",
-			name:"Prototypes Site",
-			frontBack:false
-		},
-		{
-			id:13,
-			img:Home,
-			linkLive:"https://mohmad012.github.io/Real-Estate-Site/",
-			linkGithub:"https://github.com/Mohmad012/Real-Estate-Site",
-			name:"Home Design Site",
-			frontBack:false
-		},
-		{
-			id:14,
-			img:Tech,
-			linkLive:"https://mohmad012.github.io/bootstrap3-1/",
-			linkGithub:"https://github.com/Mohmad012/bootstrap3-1",
-			name:"Tech Site",
-			frontBack:false
-		},
+		{id:1,img:amazon,linkLive:"https://amazon-app-self.vercel.app/",linkGithub:"https://github.com/Mohmad012/Amazon_App",name:"Amazon App",frontBack:false},
+		{id:2,img:tiktok,linkLive:"https://tiktok-f8fc2.web.app/",frontGithub:"https://github.com/Mohmad012/TikTok_frontend",backGithub:"https://github.com/Mohmad012/TikTok_backend",name:"TikTok App",frontBack:true},
+		{id:3,img:tinder,linkLive:"https://mohmad012.github.io/Tinder_frontend/",frontGithub:"https://github.com/Mohmad012/Tinder_frontend",backGithub:"https://github.com/Mohmad012/Tinder_backend",name:"Tinder App",frontBack:true},
+		{id:4,img:shopStore,linkLive:"https://store-shop-phi.vercel.app/",linkGithub:"https://github.com/Mohmad012/Store_Shop",name:"Shop Store",frontBack:false},
+		{id:5,img:TV,linkLive:"https://mohmad012.github.io/TV_Search_Flix/",linkGithub:"https://github.com/Mohmad012/TV_Search_Flix",name:"TV Search Flims",frontBack:false},
+		{id:6,img:Todo,linkLive:"https://mohmad012.github.io/TodoList/",linkGithub:"https://github.com/Mohmad012/TodoList",name:"Todo List",frontBack:false},
+		{id:7,img:Bookmark,linkLive:"./Projects/Bookmark/index.html",linkGithub:"https://github.com/Mohmad012/Portfolio/tree/main/Projects/Bookmark",name:"Bookmark Site",frontBack:false},
+		{id:8,img:Car,linkLive:"https://mohmad012.github.io/car-site/",linkGithub:"https://github.com/Mohmad012/car-site",name:"Car Site",frontBack:false},
+		{id:9,img:Composition,linkLive:"https://mohmad012.github.io/Sass_Pugjs_Project/",linkGithub:"https://github.com/Mohmad012/Sass_Pugjs_Project",name:"Composition Site",frontBack:false},
+		{id:10,img:Special,linkLive:"https://mohmad012.github.io/web_pure_Javascript/",linkGithub:"https://github.com/Mohmad012/web_pure_Javascript",name:"Special Design",frontBack:false},
+		{id:11,img:Elite,linkLive:"https://mohmad012.github.io/Bootstrap4/",linkGithub:"https://github.com/Mohmad012/Bootstrap4",name:"Elite Corp Site",frontBack:false},
+		{id:12,img:Prototypes,linkLive:"https://mohmad012.github.io/bootstrap3-2/",linkGithub:"https://github.com/Mohmad012/bootstrap3-2",name:"Prototypes Site",frontBack:false},
+		{id:13,img:Home,linkLive:"https://mohmad012.github.io/Real-Estate-Site/",linkGithub:"https://github.com/Mohmad012/Real-Estate-Site",name:"Home Design Site",frontBack:false},
+		{id:14,img:Tech,linkLive:"https://mohmad012.github.io/bootstrap3-1/",linkGithub:"https://github.com/Mohmad012/bootstrap3-1",name:"Tech Site",frontBack:false},
 	]
-
-	console.log(allData , "allData")
 
   return (
 	<div className="work">
@@ -151,9 +49,9 @@ const Projects = () => {
 
 			<div className="projects">
 				{allData ? allData.map((item) => (
-			      <div className="item">
+			      <div className="item" key={item.id}>
 			        <a className="views" href={item.linkLive} target="_blank">
-			          <img src={item.img} alt="Project" />
+			          <img src={item.img} alt={item.name} />
 			        </a>
 			        <a href={item.linkLive} className="btn-light" target="_blank">
 			          <i className="fas fa-eye"></i> {item.name}
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,14 +1,83 @@
 import React from "react";
 import "./Skills.css";
 import { Link, withRouter } from "react-router-dom";
-
+import ProgressBar from 'react-animated-progress-bar';
+import Tech from "./tec.jpg"
 const Skills = () => {
 
+  const allSkills = [
+  	{id:1, perc:"100", skill: "HTML"},
+  	{id:2, perc:"97", skill: "HTML5"},
+  	{id:3, perc:"97", skill: "CSS"},
+  	{id:4, perc:"95", skill: "CSS3"},
+  	{id:5, perc:"90", skill: "Pug JS"},
+  	{id:6, perc:"97", skill: "SCSS"},
+  	{id:7, perc:"95", skill: "Bootstrap"},
+  	{id:8, perc:"90", skill: "Tailwind CSS"},
+  	{id:9, perc:"95", skill: "Javascript"},
+  	{id:11, perc:"97", skill: "Git"},
+  	{id:12, perc:"90", skill: "Gulp JS"},
+  	{id:13, perc:"95", skill: "Material UI"},
+  	{id:14, perc:"90", skill: "AJAX"},
+  	{id:15, perc:"95", skill: "React JS"},
+	{id:16, perc:"90", skill: "Redux"},
+	{id:17, perc:"95", skill: "Next JS"},
+	{id:18, perc:"80", skill: "Webpack"},
+	{id:19, perc:"80", skill: "Express JS"},
+	{id:20, perc:"80", skill: "Node JS"},
+	{id:21, perc:"80", skill: "Mongodb"},
+
+  ]
+
   return (
-	<div className="bg-dark">
-		Skills
+	<div className="skills">
+
+		<main id="skills">
+		    <h1 className="lg-heading">
+		      My
+		      <span className="text-sec">Skills</span>
+		    </h1>
+
+		    <h2 className="sm-heading">
+		      Check out of my Skills...
+		    </h2>
+
+			<div className="skills-container">
+			      <div className="item">
+			      	{allSkills ? allSkills.map((item) => (
+				      	<div className="progress-bar" key={item.id}>
+							<h3>{item.skill}</h3>
+							<ProgressBar
+							          width="500px"
+							          height="10px"
+							          fontColor="white"
+							          trackWidth="10"
+							          percentage={item.perc}
+							          trackPathColor="black"
+							          trackBorderColor="black"
+							          hollowBackgroundColor="#333333"
+							          defColor={{
+							            fair: 'orangered',
+							            good: 'yellow',
+							            excellent: '#4caf50',
+							            poor: 'red',
+							          }}
+							          
+							/>
+				      	</div>
+			      	)) : <p>Loading......</p>}
+
+			      </div>
+
+			</div>
+		</main>
+		<footer id="main-footer">
+		Copyright &copy; 2021 Updated By <span> Mohmad Gamal </span>
+		</footer>
 	</div>
   );
 }
 
 export default Skills;
+
+
